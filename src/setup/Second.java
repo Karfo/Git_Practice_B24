00757de43cc205d70ad0b4d6921a1ffaf4de9573
Karfo <kv.fomicheva@gmail.com>
2021-10-03 14:49:16 -0400

    added more pages

---
+++ src/setup/Second.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class Second {
+    public void m(){
+        System.out.println("first method");
+    }
+    public  void m(int a){
+
+    }
+    public void m(String s){
+
+    }
+}
